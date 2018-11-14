0688bb67585f531b9c8a17fb48d046fcb0d0327d
Cao Viet Dung <vdung91@gmail.com>
2018-11-14 13:54:03 +0900

    chore: Add Swift package manager manifest

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Sip",
+    products: [
+        .library(
+            name: "Sip",
+            targets: ["Sip"]),
+        .executable(
+            name: "SipGen",
+            targets: ["SipGen"]),
+    ],
+    targets: [
+        .target(
+            name: "Sip",
+            dependencies: []),
+        .target(
+            name: "SipGen",
+            dependencies: []),
+        .testTarget(
+            name: "SipTests",
+            dependencies: ["Sip"]),
+    ]
+)
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import SipTests
+
+var tests = [XCTestCaseEntry]()
+tests += SipTests.allTests()
+XCTMain(tests)
